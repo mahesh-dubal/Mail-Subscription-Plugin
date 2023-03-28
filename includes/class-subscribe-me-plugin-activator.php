52613a1e43cff984901c2f5814d812a6e0a94293
mahesh4320 <mahesh.dubal@wisdmlabs.com>
2023-03-28 16:23:38 +0530

    boilerplate created

---
+++ includes/class-subscribe-me-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://mahesh-d.wisdmlabs.net
+ * @since      1.0.0
+ *
+ * @package    Subscribe_Me_Plugin
+ * @subpackage Subscribe_Me_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Subscribe_Me_Plugin
+ * @subpackage Subscribe_Me_Plugin/includes
+ * @author     Mahesh Dubal <mahesh.dubal@wisdmlabs.com>
+ */
+class Subscribe_Me_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-subscribe-me-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://mahesh-d.wisdmlabs.net
+ * @since      1.0.0
+ *
+ * @package    Subscribe_Me_Plugin
+ * @subpackage Subscribe_Me_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Subscribe_Me_Plugin
+ * @subpackage Subscribe_Me_Plugin/includes
+ * @author     Mahesh Dubal <mahesh.dubal@wisdmlabs.com>
+ */
+class Subscribe_Me_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
