fdd7394f9d3e7bbdffecdd40e3d7518ac3122f7a
mahesh4320 <mahesh.dubal@wisdmlabs.com>
2023-03-28 17:01:56 +0530

    action added to create menu page

--- includes/class-subscribe-me-plugin.php
+++ includes/class-subscribe-me-plugin.php
@@ -157,6 +157,8 @@ class Subscribe_Me_Plugin {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
+		$this->loader->add_action('admin_menu', $plugin_admin , 'my_add_menu_pages');
+
 	}
 
 	/**
