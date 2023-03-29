c54764241eff7ad6b12bc85ebe8774b75e762787
mahesh4320 <mahesh.dubal@wisdmlabs.com>
2023-03-29 10:45:44 +0530

    hook added to save mail

--- includes/class-subscribe-me-plugin.php
+++ includes/class-subscribe-me-plugin.php
@@ -185,7 +185,7 @@ class Subscribe_Me_Plugin {
 		//To display shortcode on frontpage in header section
 		$this->loader->add_action('wp_head', $plugin_public, 'add_shortcode_to_header');
 
-		//To check mail
+		//To save mail
 		$this->loader->add_action('init', $plugin_public,'save_subscriber_email');
 
 
